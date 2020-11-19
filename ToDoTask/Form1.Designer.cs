3274e2f7746927e0513346f9b854770607197f75
Black Ghost <mcramiel@gmail.com>
2020-11-19 16:28:39 +0200

    Create Main Form

--- ToDoTask/Form1.Designer.cs
+++ ToDoTask/Form1.Designer.cs
@@ -28,13 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_ShowAll = new System.Windows.Forms.Button();
+            this.btn_Remove = new System.Windows.Forms.Button();
+            this.btn_Edit = new System.Windows.Forms.Button();
+            this.btn_AddNew = new System.Windows.Forms.Button();
+            this.textBox_Description = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(611, 221);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // btn_ShowAll
+            // 
+            this.btn_ShowAll.Location = new System.Drawing.Point(650, 99);
+            this.btn_ShowAll.Name = "btn_ShowAll";
+            this.btn_ShowAll.Size = new System.Drawing.Size(75, 23);
+            this.btn_ShowAll.TabIndex = 10;
+            this.btn_ShowAll.Text = "Show all";
+            this.btn_ShowAll.UseVisualStyleBackColor = true;
+            // 
+            // btn_Remove
+            // 
+            this.btn_Remove.Location = new System.Drawing.Point(650, 70);
+            this.btn_Remove.Name = "btn_Remove";
+            this.btn_Remove.Size = new System.Drawing.Size(75, 23);
+            this.btn_Remove.TabIndex = 9;
+            this.btn_Remove.Text = "Remove";
+            this.btn_Remove.UseVisualStyleBackColor = true;
+            // 
+            // btn_Edit
+            // 
+            this.btn_Edit.Location = new System.Drawing.Point(650, 41);
+            this.btn_Edit.Name = "btn_Edit";
+            this.btn_Edit.Size = new System.Drawing.Size(75, 23);
+            this.btn_Edit.TabIndex = 8;
+            this.btn_Edit.Text = "Edit";
+            this.btn_Edit.UseVisualStyleBackColor = true;
+            // 
+            // btn_AddNew
+            // 
+            this.btn_AddNew.Location = new System.Drawing.Point(650, 12);
+            this.btn_AddNew.Name = "btn_AddNew";
+            this.btn_AddNew.Size = new System.Drawing.Size(75, 23);
+            this.btn_AddNew.TabIndex = 7;
+            this.btn_AddNew.Text = "Add new";
+            this.btn_AddNew.UseVisualStyleBackColor = true;
+            // 
+            // textBox_Description
+            // 
+            this.textBox_Description.Location = new System.Drawing.Point(13, 240);
+            this.textBox_Description.Multiline = true;
+            this.textBox_Description.Name = "textBox_Description";
+            this.textBox_Description.Size = new System.Drawing.Size(610, 80);
+            this.textBox_Description.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 336);
+            this.Controls.Add(this.textBox_Description);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_ShowAll);
+            this.Controls.Add(this.btn_Remove);
+            this.Controls.Add(this.btn_Edit);
+            this.Controls.Add(this.btn_AddNew);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_ShowAll;
+        private System.Windows.Forms.Button btn_Remove;
+        private System.Windows.Forms.Button btn_Edit;
+        private System.Windows.Forms.Button btn_AddNew;
+        private System.Windows.Forms.TextBox textBox_Description;
     }
 }
 
